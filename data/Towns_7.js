3a861ef4b17145e5f47aaf60cacf7eb97a353104
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-01-26 14:18:09 +0530

    Add files via upload

---
+++ data/Towns_7.js
@@ -0,0 +1,14 @@
+var json_Towns_7 = {
+"type": "FeatureCollection",
+"name": "Towns_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Anai Mudi" }, "geometry": { "type": "Point", "coordinates": [ 78.190227066958798, 10.59349103692583, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Karur" }, "geometry": { "type": "Point", "coordinates": [ 78.078682566986416, 10.95825421839133, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Chinna Dharapuram" }, "geometry": { "type": "Point", "coordinates": [ 77.847033299126068, 10.853760640682349, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Pallapatti" }, "geometry": { "type": "Point", "coordinates": [ 77.895018898058794, 10.7219320164089, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Velayuthampalayam" }, "geometry": { "type": "Point", "coordinates": [ 78.001090227296842, 11.07666552309558, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kulithalai" }, "geometry": { "type": "Point", "coordinates": [ 78.421103844580415, 10.9392345160822, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Thogaimalai" }, "geometry": { "type": "Point", "coordinates": [ 78.411318081579623, 10.72471288870504, -999999.0 ] } }
+]
+}
